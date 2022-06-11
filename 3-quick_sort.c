77d0a4ced4ef24cf61cbec5f0731e22cac482259
FloLys <4440@holbertonschool.com>
2022-06-11 10:28:23 -0700

    update answer format

---
+++ 3-quick_sort.c
@@ -0,0 +1,24 @@
+#include "sort.h"
+
+/**
+* quick_sort - sort an array of ints with quick sort using Lomuto partition
+* and print the array after each swap. The pivot should always be the
+* last element of the partition being sorted.
+* @array: array
+* @size: size
+*/
+void quick_sort(int *array, size_t size)
+{
+	/*example O(nlog(n)) quiz*/
+    int i;
+    int j;
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 1; j < n; j = j * 2)
+        {
+            printf("[%d] [%d]\n", i, j);
+        }
+    }
+
+}
